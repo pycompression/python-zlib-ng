dd6d169e1a8fe0fc125693ada69b5bb603933dc1
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-01-23 10:25:35 +0100

    Add __init__.py to properly init package

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
-# Copyright (c) 2020 Leiden University Medical Center
 # Copyright (c) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
-# 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022
+# 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023
 # Python Software Foundation; All Rights Reserved
 
 # This file is part of python-zlib-ng which is distributed under the
