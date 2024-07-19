4edc533f7a675b2870b787f8b2ed8e206eaac2f1
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2024-07-19 07:08:58 +0200

    Use shutil.copyfileobj for fileobj copying

--- src/zlib_ng/gzip_ng.py
+++ src/zlib_ng/gzip_ng.py
@@ -22,6 +22,7 @@ import argparse
 import gzip
 import io
 import os
+import shutil
 import struct
 import sys
 import time
@@ -302,11 +303,7 @@ def main():
     global READ_BUFFER_SIZE
     READ_BUFFER_SIZE = args.buffer_size
     try:
-        while True:
-            block = in_file.read(args.buffer_size)
-            if block == b"":
-                break
-            out_file.write(block)
+        shutil.copyfileobj(in_file, out_file, args.buffer_size)
     finally:
         if in_file is not sys.stdin.buffer:
             in_file.close()
