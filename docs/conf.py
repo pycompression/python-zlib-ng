83ab81b940bc088384851e3eccbbd4a23a3f5261
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2024-09-25 08:45:28 +0200

    Fix readthedocs build

--- docs/conf.py
+++ docs/conf.py
@@ -4,26 +4,19 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-from distutils.dist import DistributionMetadata
-from pathlib import Path
-
-import pkg_resources
+import zlib_ng
 
 # -- Project information -----------------------------------------------------
-
-# Get package information from the installed package.
-package = pkg_resources.get_distribution("zlib-ng")
-metadata_file = Path(package.egg_info) / Path(package.PKG_INFO)
-metadata = DistributionMetadata(path=str(metadata_file))
 
 project = 'python-zlib-ng'
 copyright = '2023, Leiden University Medical Center'
 author = 'Leiden University Medical Center'
 
+
 # The short X.Y version
-version = package.parsed_version.base_version
+version = zlib_ng.__version__
 # The full version, including alpha/beta/rc tags
-release = package.version
+release = zlib_ng.__version__
 
 
 # -- General configuration ---------------------------------------------------
